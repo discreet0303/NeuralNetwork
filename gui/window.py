13d0f8d3c0609e4a23bba8420413c30636abd419
Unknown <qhespqhesp@gmail.com>
2018-09-26 17:44:16 +0800

    Initial commit

---
+++ gui/window.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+def initWindow():
+    win = tk.Tk()
+    win.title("My First Tk GUI")
+    win.resizable(0,0)
+    win.mainloop()
+
+if __name__ == "__main__":
---
+++ main.py
@@ -0,0 +1,10 @@
+# import sys
+# sys.path.append(0, sys.path[0]+'\gui')
+# # print(sys.path[0])
+
+from gui import window
+
+if __name__ == "__main__":
+    window.initWindow()
+    print( '__name__:' + __name__)
+#     window()
