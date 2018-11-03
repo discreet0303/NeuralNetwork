bba42230354a7ca56f0a8053515549e8040fedbc
Unknown <qhespqhesp@gmail.com>
2018-11-03 10:28:19 +0800

    update back propagate

--- main.py
+++ main.py
@@ -6,27 +6,87 @@
 
 from src.nerual.PerceptronV2 import PerceptronV2
 import numpy as np
+
+xorData = [
+    [[-1, 0, 0], 0],
+    [[-1, 0, 1], 1],
+    [[-1, 1, 0], 1],
+    [[-1, 1, 1], 0],
+]
+
 def mulitPerceptron():
-    test = [
+    multiPerceptronModel = [
         [PerceptronV2(), PerceptronV2()],
         [PerceptronV2()]
     ]
 
-    lastLevelIndex = len(test) - 1
+    multiPerceptronModel[0][0].setWeight([-1.2, 1, 1])
+    multiPerceptronModel[0][1].setWeight([0.3, 1, 1])
+    multiPerceptronModel[1][0].setWeight([0.5, 0.4, 0.8])
 
-    print(lastLevelIndex)
+    inputTemp = xorData[3]
 
-    test[0][0].setWeight([-1.2, 1, 1])
-    test[0][1].setWeight([0.3, 1, 1])
-    test[1][0].setWeight([0.5, 0.4, 0.8])
-
-    for level in test:
+    for levelCount, level in enumerate(multiPerceptronModel):
         for perceptron in level:
-            print(perceptron.getWeight())
+            if levelCount == 0:
+                perceptron.setInputData(inputTemp)
+            else:
+                EOutput = getLevelPerceptronOutput(multiPerceptronModel[levelCount - 1])
+                perceptron.setInputData([EOutput, 0])
+    
+    last = 0
+    for levelIndex, level in enumerate(multiPerceptronModel[::-1]):
+        # print(levelIndex)
+        # print(backPerceptron)
+        # print('-----------')
+        
+        for backPerceptronIndex, backPerceptron in enumerate(level):
+            if levelIndex == 0:
+                backPerceptron.setBackPropagate(True, 0, 0)
+                last = backPerceptron.getBackPropagate()
+            else:
+                if backPerceptronIndex == 0:
+                    backPerceptron.setBackPropagate(False, last, 0.4)
+                elif backPerceptronIndex == 1:
+                    backPerceptron.setBackPropagate(False, last, 0.8)
+
+                # print(backPerceptron.getBackPropagate())
+
+            backPerceptron.resetWeight()
+
+
+
+
+    # print('10----------Start')
+    # # print(multiPerceptronModel[1][0].getEOutput())
+    # # print(test[1][0].getWeight())
+    # test[1][0].setBackPropagate(True, 0, 0)
+    # last = test[1][0].getBackPropagate()
+    # test[1][0].resetWeight()
+    # print('10----------')
+
+    # print('00----------Start')
+    # # print(test[0][0].getEOutput())
+    # # print(test[0][0].getWeight())
+    # test[0][0].setBackPropagate(False, last, 0.4)
+    # test[0][0].resetWeight()
+    # print('00----------End')
+
+    # print('01----------Start')
+    # # print(test[0][1].getEOutput())
+    # # print(test[0][1].getWeight())
+    # test[0][1].setBackPropagate(False, last, 0.8)
+    # test[0][1].resetWeight()
+    # print('01----------End')
+
     regexEValue()
-    # a = PerceptronV2()
-    # print(a.getWeight())
-    # print(a.sigmoidal())
+
+def getLevelPerceptronOutput(perceptronItems):
+    EOutputData = [-1]
+    for item in perceptronItems:
+        EOutputData.append(item.getEOutput())
+    return EOutputData
+
 
 def regexEValue():
     allEValue = [1, 2, -1]
@@ -41,8 +101,6 @@ def regexEValue():
                 'maxRange': (index + 1) * num
             }
         })
-
-    print(temp)
 
 
 if __name__ == "__main__":
--- src/nerual/PerceptronV2.py
+++ src/nerual/PerceptronV2.py
@@ -3,21 +3,29 @@ import numpy as np
 import math
 
 class PerceptronV2:
-
     def __init__(self):
         print('PerceptronV2')
         
-        self.__DATA = [ 
-            [-1, 0, 0],
-            [-1, 0, 1],
-            [-1, 1, 0],
-            [-1, 1, 1]
-        ]
-
-        self.__DATA_LENGTH = len(self.__DATA[0]) - 1
-        self.__DATA_LENGTH_WITH_PREFIX = self.__DATA_LENGTH + 1
-
+        self.__DATA = []
+        self.__EOutput = -100
+        self.__LEARN_RATE = 0.5
+        self.__BACK_PROPAGATE = 0
         # self.randomWeight()
+
+    def setInputData(self, inputData):
+        self.__DATA = inputData
+        self.__DATA_LENGTH = len(inputData[0]) - 1
+        self.__DATA_LENGTH_WITH_PREFIX = len(inputData[0])
+        self.setEOutput()
+
+    def setEOutput(self):
+        expNum = np.dot(self.__DATA[0], self.__W)
+        sigmoidalNum = 1 / (1 + math.exp(-1 * expNum))
+        self.__EOutput = sigmoidalNum
+    
+    def getEOutput(self):
+        print(self.__EOutput)
+        return self.__EOutput
 
     def randomWeight(self):
         # self.__W = []
@@ -26,13 +34,34 @@ class PerceptronV2:
         self.__W = [-1.2, 1, 1]
 
     def getWeight(self):
+        print(self.__W)
         return self.__W
 
     def setWeight(self, w):
         self.__W = w
 
-    def sigmoidal(self):
-        expNum = np.dot(self.__DATA[3], self.__W)
-        sigmoidalNum = 1 / (1 + math.exp(-1 * expNum))
+    def setBackPropagate(self, outputLevel, preBackPropagate, weightValue):
+        if outputLevel:
+            self.__BACK_PROPAGATE = (self.__DATA[1] - self.__EOutput) * self.__EOutput * (1 - self.__EOutput)
+            print('output level')
+        else:
+            self.__BACK_PROPAGATE = self.__EOutput * (1 - self.__EOutput) * preBackPropagate * weightValue
+            print('preBackPropagate level')
+        # print(self.__EOutput)
+        # print(preBackPropagate)
+        # print(weightValue)
+        # print(self.__BACK_PROPAGATE)
 
-        return sigmoidalNum
+    def getBackPropagate(self):
+        return self.__BACK_PROPAGATE
+
+    def resetWeight(self):
+        temp = []
+
+        for index, item in enumerate(self.__W):
+            val = item + 0.5 * self.__BACK_PROPAGATE * self.__DATA[0][index]
+            temp.append(val)
+        
+        self.__W = temp
+        print(self.__W)
+
